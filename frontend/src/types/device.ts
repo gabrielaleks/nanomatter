afc66f6fca68e870690a08125dff4bdf68938a8c
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2026-05-05 12:08:51 -0300

    Update interfaces to match what's returned by the controller

--- frontend/src/types/device.ts
+++ frontend/src/types/device.ts
@@ -1,3 +1,22 @@
-export interface Device {
-  id: string
+type BaseDevice = {
+  id: number
+  name: string
+  factoryName: string
+  reachable: boolean
+  on: boolean
+  brightness: number
 }
+
+export type Device =
+  | (BaseDevice & {
+    colorMode: 'hue-saturation'
+    hue: number
+    saturation: number
+    colorTemperature?: never
+  })
+  | (BaseDevice & {
+    colorMode: 'color-temperature'
+    colorTemperature: number
+    hue?: never
+    saturation?: never
+  })
--- frontend/src/types/room.ts
+++ frontend/src/types/room.ts
@@ -1,7 +1,12 @@
 import type { Device } from "./device"
 
 export interface Room {
-  id: string
+  id: number
   name: string
   devices: Device[]
+}
+
+export interface Rooms {
+  assigned: Room[]
+  unassigned: Device[]
 }
